1a651df52729a618b2ba3b14d5b1641e4a9c8175
EduardomdRosa <101217651+EduardomdRosa@users.noreply.github.com>
2023-02-07 10:45:32 -0300

    NetFlix Clone

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './Header.css'
+
+export default ({black}) => {
+    return (
+        <header className={black ? 'black' : ''}>
+            <div className="header--logo">
+                <a href="/">
+                    <img src="https://images.ctfassets.net/4cd45et68cgf/7LrExJ6PAj6MSIPkDyCO86/542b1dfabbf3959908f69be546879952/Netflix-Brand-Logo.png?w=684&h=456"></img>
+                </a>
+            </div>
+            <div className="header--user">
+                <a href="/">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"></img>
+                </a>
+            </div>
+        </header>
+
+    );
+}
